58905c3704bbb25966e8c726885a96408d2d96eb
Jacob Smith <jsmith0361@jh-mba158-19.d214.org>
2018-12-11 11:40:11 -0600

    Initial Commit

---
+++ FinalAppHomeworkCompiler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalAppHomeworkCompiler
+//
+//  Created by Jacob Smith on 12/11/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
